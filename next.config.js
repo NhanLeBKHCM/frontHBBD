dff77daf3724ade82585d23b9f1f91f63891173d
Nhân Lê <nhan.le1407bkhcm@hcmut.edu.vn>
2023-08-17 14:17:59 +0700

    first

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    BACKEND_HOST: "https://back-hbbd-nhanlebkhcm.vercel.app",
+    // BACKEND_HOST: "https://back-hbbd-nhanlebkhcm.vercel.app",
+    BACKEND_HOST: "https://back-hbbd-ea53df6e51fd.herokuapp.com",
     // BACKEND_HOST: "http://192.168.0.110:3001",
   },
 };
